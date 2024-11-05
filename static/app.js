90929ec057665bc5dbd85c3b9b8e29f2ffd89a6b
Jin Cao <jc.cao@live.com>
2024-11-04 21:36:43 -0500

    Add click comment text to collapse.

--- static/app.js
+++ static/app.js
@@ -9,6 +9,21 @@ function toggleSearch() {
     }
 }
 
+function addCommentToggleEvent() {
+    let threads = document.getElementsByClassName("thread");
+    for (let thread of threads) {
+        thread.addEventListener('click', function (e) {
+            let tagName = e.target.tagName;
+            let comment = e.target.closest('.comment_body');
+            if (tagName == 'P' && comment && window.getSelection().toString().length === 0) {
+                // parent node is the 'details' node.
+                comment.parentNode.removeAttribute('open');
+            }
+        });
+    }
+}
+
 window.onload = function () {
     document.getElementById("search_icon").onclick = toggleSearch;
+    addCommentToggleEvent();
 }
